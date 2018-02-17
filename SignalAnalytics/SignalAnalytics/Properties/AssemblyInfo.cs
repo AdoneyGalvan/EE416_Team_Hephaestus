d1245349005b13abf2bc14c5b779aa957f6ea068
Ryan Epperson <rz.eppy97@gmail.com>
2018-02-16 20:03:14 -0800

    Signal Analytic Functions V1.1, with drivers

--- SignalAnalytics/SignalAnalytics/Properties/AssemblyInfo.cs
+++ SignalAnalytics/SignalAnalytics/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Signal.Analytics")]
+[assembly: AssemblyTitle("SignalAnalytics")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Signal.Analytics")]
+[assembly: AssemblyProduct("SignalAnalytics")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5dd79086-b519-4d0e-8a73-eabca26df9b7")]
+[assembly: Guid("4846e1a2-dfb6-440b-9568-b8fd12fb66f3")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ SignalAnalytics/SignalAnalytics/Signals.cs
@@ -0,0 +1,147 @@
+﻿//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//Remove if not debugging, keeps console open
+#define DEBUG
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics;
+using MathNet.Numerics.IntegralTransforms;
+
+
+
+namespace SignalAnalytics
+{
+    class SigFunctions
+    {
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        //Computes Radix 2 FFT
+        //Inputs: Complex32 array of signal samples
+        //        length of samples array
+        //Output: none
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        public void FFT(ref double[] signalSamples, int length)
+        {
+            Fourier.ForwardReal(signalSamples, length);
+
+            //Normalize FFT Results
+            //for (int i = 0; i < length; i++)
+            //{
+            //    signalSamples[i] = signalSamples[i] / length;
+            //}
+        }
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        //Computes RMS value for a signal samples
+        //Inputs: Complex32 array of signal samples
+        //        Int of length of array
+        //Output: none
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        public double RMS(double[] signalSamples, int length)
+        {
+            double RMS = 0;
+            for (int i = 0; i < length; i++)
+            {
+                RMS += (signalSamples[i] * signalSamples[i]);
+            }
+
+            RMS = ((double)1 / length) * RMS;
+            return Math.Sqrt(RMS);
+        }
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        //Scales the indeces to be frequencies of FFT
+        //Inputs: ref int[] indeces, int length, int freq
+        //Output: none
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        public void IndexScaling(ref int[] indeces, int length, int freq)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                indeces[i] = i * (freq / length);
+                indeces[i] -= (freq / 2);
+            }
+        }
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        //Generates indeces for the FFT
+        //Inputs: length of samples
+        //Output: none
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        public void GenerateIndeces(int LengthSamples, int[] indeces)
+        {
+            indeces[0] = 0;
+            for (int i = 0; i < LengthSamples; i++)
+            {
+                indeces[i] = i;
+            }
+        }
+
+        public double[] SignalSamples { get; set; }
+        public int LengthSamples { get; set; }
+        public int SampleFreq { get; set; }
+        public int[] indeces;
+    }
+
+
+    //Test driver for SigFunctions Class
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            double[] deg = new double[1000];
+            deg[0] = 0;
+
+            double[] samples = new double[1000];
+
+            for (int i = 1; i < 1000; i++)
+            {
+                deg[i] = deg[i - 1] + 0.01;
+
+                samples[i] = Math.Sin(deg[i]);
+
+                //Console.WriteLine(deg[i]);
+                //Console.WriteLine(samples[i]);
+                //Console.Write('\n');
+            }
+                //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+                SigFunctions Sig = new SigFunctions
+                {
+                SignalSamples = samples,
+                LengthSamples = 1000,
+                SampleFreq = 1000,
+                indeces = new int[1000]
+                };
+
+
+            Sig.GenerateIndeces(Sig.LengthSamples, Sig.indeces);
+            //Sig.FFT(ref amp, 14);
+
+            double RMS_val = Sig.RMS(samples, Sig.LengthSamples);
+
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+
+            Console.WriteLine(RMS_val);
+
+            //foreach (double element in amp) Console.WriteLine(element);
+            //Console.WriteLine();
+            //foreach (double element in Sig.indeces) Console.WriteLine(element);
+
+
+
+
+            #if DEBUG
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+            #endif
+        }
+    }
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+}
