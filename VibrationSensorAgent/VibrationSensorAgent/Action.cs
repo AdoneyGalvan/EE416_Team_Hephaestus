438c3e24e775b40f1f385de6906595646b8a8d0c
Jerome <jeromerayy@gmail.com>
2018-03-27 14:46:13 -0700

    Added relfex agent to repository

---
+++ VibrationSensorAgent/VibrationSensorAgent/Action.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VibrationSensorAgent
+{
+    public class Action
+    {
+        private bool _notify;    // Notification boolean
+
+        public Action() { }      // End constructor
+
+        public bool Notify
+        {
+            get { return _notify; }
+            set { _notify = value; }
+        }   // End property
+
+        public void PrintAction(Action action)
+        {
+            if (action.Notify == true)
+            {
+                // Notify the user
+                Console.WriteLine("Notify maintenance!");
+            }
+            else
+            {
+                // Notify the user
+                Console.WriteLine("Pass");
+            }
+        }   // End function
+    }   // End class
+}
---
+++ VibrationSensorAgent/VibrationSensorAgent/agent.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VibrationSensorAgent
+{
+    public class Agent
+    {
+        private int _breakthroughCount; // Number of times 
+        private double _average;        // Running average of data
+        private double _threshold;      // Threshold for each data point
+        private Stack<Action> _actions; // Stack to hold the actions
+
+        public Agent()
+        {
+            _breakthroughCount = 5;
+            _average = 0;
+            _threshold = 100;
+            _actions = new Stack<Action>();
+        }   // End constructor
+
+        public int BreakthroughCount
+        {
+            get { return _breakthroughCount; }
+            set { _breakthroughCount = value; }
+        }   // End property
+
+        public double Average
+        {
+            get { return _average; }
+            set { _average = value; }
+        }   // End property
+
+        public double Threshold
+        {
+            get { return _threshold; }
+            set { _threshold = value; }
+        }   // End property
+
+        public Action Process(double[] dataSample)
+        {
+            int count = 0;
+
+            if (_actions.Count == 0)
+            {
+                for (int i = 0; i < dataSample.Length; i++)
+                {
+                    if (dataSample[i] > _threshold)
+                    {
+                        count++;    // Increment number of times past threshold
+                        if (count > _breakthroughCount)
+                        {
+                            // Notify the user
+                            _actions.Push(new Action() { Notify = true });
+                            break;
+                        }
+                    }
+                    else
+                        _actions.Push(new Action() { Notify = false });
+                }
+            }
+            Action action = _actions.Peek();
+            _actions.Pop();
+            return action;
+        }   // End function
+    }   // End class
+}
